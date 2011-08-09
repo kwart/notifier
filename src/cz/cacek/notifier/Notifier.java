03be5c95aa5c00ce3df77f1ebb5f79d0fc67ae14
Josef Cacek <josef.cacek@gmail.com>
2011-08-09 18:03:32 +0200

    license info

--- src/cz/cacek/notifier/Notifier.java
+++ src/cz/cacek/notifier/Notifier.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Josef Cacek
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package cz.cacek.notifier;
 
 import java.awt.AWTException;
@@ -27,7 +43,6 @@ import com.sun.net.httpserver.HttpServer;
  * @author Josef Cacek
  */
 public class Notifier implements HttpHandler {
-  private static final long serialVersionUID = 1L;
 
   public static final int DEFAULT_PORT = 8811;
   public static final String DEFAULT_SOUND_PROP = "win.sound.asterisk";
